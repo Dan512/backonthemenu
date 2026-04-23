40afe13ab20a5156ed447fc322363e7300b6a126
Dan512 <Dan512@users.noreply.github.com>
2026-04-23 09:10:19 -0500

    Initial deploy

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = "botm-v1";
+const ASSETS = ["/", "/index.html"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit ||
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match("/"))
+    )
+  );
+});
